caffdef1b8a6175c3efe1f1640865e2d944d8d19
Miguelmc5 <mmartinezcosta@danielcastelao.org>
2020-01-14 09:58:04 +0100

    excepcion

---
+++ boletin14/TemperaturaErradaExcepcion.java
@@ -0,0 +1,17 @@
+package boletin14;
+
+/**
+ *
+ * @author mmartinezcosta
+ */
+public class TemperaturaErradaExcepcion extends Exception{
+
+    public TemperaturaErradaExcepcion(){
+        super();
+    
+}
+    public TemperaturaErradaExcepcion(String mensaxe){
+        super(mensaxe);
+    }
+         
+}
